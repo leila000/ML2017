b5020c6c75043c5ca6684d5829afc6c0a048f9ee
yjkim9306 <rladpwl9306@naver.com>
2017-06-16 23:33:07 +0900

    Add files via upload

---
+++ ANN/Reference code/datahandler.py
@@ -0,0 +1,131 @@
+import numpy as np
+from numpy import linalg as la
+import os
+import data
+import six
+from tqdm import tqdm
+
+
+def normalize(data, M=None, Sd=None):
+    """ Normalize data """
+    print('Data normalization......')
+    if M == None:
+        M = np.mean(data, axis=0)        # mean
+    if Sd == None:
+        Sd = np.std(data, axis=0)        # Std
+
+    stmat = np.zeros([len(Sd), len(Sd)])
+    for i in range(0, len(Sd)):
+        stmat[i][i] = Sd[i]
+    S_inv = la.inv(np.matrix(stmat))
+    data_n = S_inv.dot((np.matrix(data - M)).T)
+    data_n = np.array(data_n.T, dtype=np.float32)
+    return data_n, M, Sd
+
+
+def pad_addnoise(data_x, data_y, mean=0.0, sd=1.0, mixratio=1.0, noiseratio=0.3):
+    """ Data padding: add noise ~ N(mean, sd) """
+    print('Data padding: adding noise to data......')
+    col, row = np.int(len(data_x) * mixratio), len(data_x[0])
+    noise = np.random.normal(mean, sd, (col, row)) * np.sqrt(noiseratio)
+    noised = np.array(data_x[0:col] * np.sqrt(1 - noiseratio) + noise, dtype=np.float32)
+    noised_x = np.append(data_x, noised, axis=0)
+    noised_y = np.append(data_y, data_y[0:col], axis=0)
+    return noised_x, noised_y
+
+
+def pad_rightleft(data_x, data_y, mixratio=1.0, ch=3):
+    """ Data padding: rightside left """
+    print('Data padding: rightside left......')
+    col, size = np.int(len(data_x) * mixratio), len(data_x[0]) / ch
+    height, width = 32, 32
+
+    rl = data_x.copy()
+    for i in tqdm(range(0, ch)):
+        for j in range(0, height):
+            r = data_x[:, i * size + j * height:i * size + j * height + width]
+            rl[:, i * size + j * height:i * size + j * height + width] = np.fliplr(r)
+    rl_x = np.append(data_x, rl[0:col], axis=0)
+    rl_y = np.append(data_y, data_y[0:col], axis=0)
+    return rl_x, rl_y
+
+
+def crop_data(data_x, data_y, imagesize=32, insize=24, stride=4, ch=3):
+    print('Data cropping: images x 3 x 3......')
+    ratio = (imagesize - insize) / stride + 1
+    imagemat, labelvec = data_x, data_y
+    nimage = len(imagemat)
+
+    cropped_x = np.zeros([nimage * ratio ** 2, insize ** 2 * ch])
+    cropped_y = np.zeros(nimage * ratio ** 2)
+    for i in tqdm(range(0, nimage)):
+        for c in range(0,ch):
+            imagemat = data_x[i][c * imagesize ** 2:(c + 1) * imagesize ** 2]
+            for j in range(0, ratio):
+                xind = j * stride
+                for k in range(0, ratio):
+                    yind = k * stride
+                    cropped_x[i * ratio**2 + j * ratio + k, c * insize ** 2:(c + 1) * insize ** 2] = np.reshape(
+                        np.reshape(imagemat, (imagesize, imagesize))[yind:yind + insize, xind:xind + insize],
+                        (1, insize ** 2))
+        cropped_y[i * ratio ** 2: (i + 1) * ratio ** 2] = np.ones(ratio ** 2).astype(np.int32) * labelvec[i]
+
+    data_x = cropped_x.astype(np.float32)
+    data_y = np.array(cropped_y, dtype=np.int32)
+    return data_x, data_y
+
+
+def process_data(augmentation=2):
+    cifar = load_data()
+    cifar['train']['x'], m, sd = normalize(cifar['train']['x'])
+    cifar['test']['x'], m, sd = normalize(cifar['test']['x'], M=m, Sd=sd)
+    if augmentation > 0:
+        cifar['train']['x'], cifar['train']['y'] = pad_rightleft(cifar['train']['x'], cifar['train']['y'],
+                                                                 mixratio=augmentation)
+    if augmentation > 1.0:
+        cifar['train']['x'], cifar['train']['y'] = pad_addnoise(cifar['train']['x'], cifar['train']['y'],
+                                                                mixratio=augmentation - 1.0)
+#    data.save_pkl(cifar, savename='cifar_processed.pkl')
+    return cifar
+
+
+def load_processed_data():
+    if not os.path.exists('cifar_processed.pkl'):
+        cifar = process_data()
+        return cifar
+    else:
+        with open('cifar_processed.pkl', 'rb') as cifar_pickle:
+            data = six.moves.cPickle.load(cifar_pickle)
+    return data
+
+
+def load_data():
+    cifar = data.load_data()
+    cifar['train']['x'] = cifar['train']['x'].astype(np.float32)
+    cifar['test']['x'] = cifar['test']['x'].astype(np.float32)
+    cifar['train']['x'] /= 255
+    cifar['test']['x'] /= 255
+    cifar['train']['y'] = np.array(cifar['train']['y'], dtype=np.int32)
+    cifar['test']['y'] = np.array(cifar['test']['y'], dtype=np.int32)
+    return cifar
+
+
+"""
+def pad_updown(data):
+    return data
+def pad_crop(data):
+    return data
+def load_processed_data():
+    if not os.path.exists('cifar_processed.pkl'):
+        cifar = process_data()
+        return cifar
+    else:
+        with open('cifar_processed.pkl', 'rb') as cifar_pickle:
+            data = six.moves.cPickle.load(cifar_pickle)
+        return data
+    imagemat = np.reshape(imagemat, (nimage, imagesize, imagesize, ch))
+#    imagemat = np.transpose(np.reshape(imagemat, (nimage, imagesize, imagesize, ch)), (0, 3, 1, 2))
+                    cropped_x[i + j * ratio + k, c * insize ** 2:(c + 1) * insize ** 2] = np.reshape(
+                        imagemat[xind:xind + insize, yind:yind + insize],
+                                                      (1, insize ** 2 * ch))
+"""
---
+++ ANN/Reference code/train_nn.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+"""Chainer example: train a multi-layer perceptron on cifar
+This is a minimal example to write a feed-forward net.
+"""
+from __future__ import print_function
+import argparse
+
+import numpy as np
+import six
+
+import chainer
+from chainer import computational_graph
+from chainer import cuda
+import chainer.links as L
+from chainer import optimizers
+from chainer import serializers
+
+import time
+import datahandler as dh
+
+import model_nn
+
+
+parser = argparse.ArgumentParser(description='Example: cifar-10')
+parser.add_argument('--data', '-d', choices=('on', 'off'),
+                    default='off', help='Data normalization and augmentation flag')
+parser.add_argument('--gpu', '-g', default=-1, type=int,
+                    help='GPU ID (negative value indicates CPU)')
+parser.add_argument('--logflag', '-l', choices=('on', 'off'),
+                    default='off', help='Writing log flag')
+parser.add_argument('--initmodel', '-m', default='',
+                    help='Initialize the model from given file')
+parser.add_argument('--net', '-n', choices=('simple', 'parallel'),
+                    default='simple', help='Network type')
+parser.add_argument('--plotflag', '-p', choices=('on', 'off'),
+                    default='off', help='Accuracy plot flag')
+parser.add_argument('--resume', '-r', default='',
+                    help='Resume the optimization from snapshot')
+parser.add_argument('--saveflag', '-s', choices=('on', 'off'),
+                    default='off', help='Save model and optimizer flag')
+args = parser.parse_args()
+
+
+# Prepare dataset
+print('load cifar-10 dataset')
+if args.data == 'on': cifar = dh.load_processed_data()
+else: cifar = dh.load_data()
+
+
+N = len(cifar['train']['x'])
+N_test = len(cifar['test']['x'])/10
+batchsize = 200
+n_epoch = 20
+
+
+# Prepare multi-layer perceptron model, defined in net.py
+n_inputs = cifar['ndim']
+n_units =100
+n_outputs = len(cifar['labels'])
+if args.net == 'simple':
+    model = L.Classifier(model_nn.cifarMLP(n_inputs, n_units, n_outputs))
+    if args.gpu >= 0:
+        cuda.get_device(args.gpu).use()
+        model.to_gpu()
+    xp = np if args.gpu < 0 else cuda.cupy
+elif args.net == 'parallel':
+    cuda.check_cuda_available()
+    model = L.Classifier(model_nn.cifarMLPParallel(n_inputs, n_units, n_outputs))
+    xp = cuda.cupy
+
+# Setup optimizer
+optimizer = optimizers.Adam()
+optimizer.setup(model)
+
+# Init/Resume
+if args.initmodel:
+    print('Load model from', args.initmodel)
+    serializers.load_hdf5(args.initmodel, model)
+if args.resume:
+    print('Load optimizer state from', args.resume)
+    serializers.load_hdf5(args.resume, optimizer)
+
+
+train_ac, test_ac, train_mean_loss, test_mean_loss = [], [], [], []
+
+# Learning loop
+stime = time.clock()
+for epoch in six.moves.range(1, n_epoch + 1):
+    print('epoch', epoch)
+
+    # training
+    perm = np.random.permutation(N)
+    sum_accuracy = 0
+    sum_loss = 0
+    for i in six.moves.range(0, N, batchsize):
+        x = chainer.Variable(xp.asarray(
+                             cifar['train']['x'][perm[i:i + batchsize]]))
+        t = chainer.Variable(xp.asarray(
+                             cifar['train']['y'][perm[i:i + batchsize]]))
+
+        # Pass the loss function (Classifier defines it) and its arguments
+        optimizer.update(model, x, t)
+
+        sum_loss += float(model.loss.data) * len(t.data)
+        sum_accuracy += float(model.accuracy.data) * len(t.data)
+
+    print('train mean loss={}, accuracy={}'.format(
+        sum_loss / N, sum_accuracy / N))
+    train_mean_loss.append(sum_loss / N)
+    train_ac.append(sum_accuracy / N)
+
+    # evaluation
+    sum_accuracy = 0
+    sum_loss = 0
+    for i in six.moves.range(0, N_test, batchsize):
+        x = chainer.Variable(xp.asarray(cifar['test']['x'][i:i + batchsize]),
+                             volatile='on')
+        t = chainer.Variable(xp.asarray(cifar['test']['y'][i:i + batchsize]),
+                             volatile='on')
+        loss = model(x, t)
+        sum_loss += float(loss.data) * len(t.data)
+        sum_accuracy += float(model.accuracy.data) * len(t.data)
+
+    print('test  mean loss={}, accuracy={}'.format(
+        sum_loss / N_test, sum_accuracy / N_test))
+    test_mean_loss.append(sum_loss / N_test)
+    test_ac.append(sum_accuracy / N_test)
+
+
+if args.logflag == 'on':
+    import log
+    etime = time.clock()
+    log.write_nn(N, N_test, n_inputs, n_units, n_outputs, batchsize, args.net,
+                 stime, etime, train_mean_loss, train_ac,
+                 test_mean_loss, test_ac, epoch, LOG_FILENAME='log.txt')
+
+
+if args.plotflag == 'on':
+    import plot
+    plot.plot_result(train_ac, test_ac, train_mean_loss, test_mean_loss,
+                     savename='result.png')
+
+
+# Save the model and the optimizer
+if args.saveflag == 'on':
+    print('save the model')
+    serializers.save_hdf5('cifar10.model', model)
+    print('save the optimizer')
+    serializers.save_hdf5('cifar10.state', optimizer)
